a261f3acadb680711d972b952e3d9c937f75050e
CompleteTechSolution <protech.thecompletetechsolution@gmail.com>
2024-09-19 21:43:09 +0600

    home pages added

--- login.js
+++ login.js
@@ -8,10 +8,23 @@ document.getElementById('button-login').addEventListener('click', function (even
    event.preventDefault();   //<---------------Vejal to beginners
    console.log('login');
    
-//    step-3: get the phone number
+//    step-3: get the phone number and the pin number
      const phoneNumber = document.getElementById('phone-number');
      const phoneNumberValue = phoneNumber.value;
-     console.log(phoneNumberValue);
+     const pinNumber = document.getElementById('pin-number');
+     const pinNumberValue = pinNumber.value;
      
+     console.log(phoneNumberValue, pinNumberValue);
+     
+    //  step-4: validation phone and pin(bad way)
+    //  this is temporary. you should do like this
+     if (phoneNumberValue === '5' && pinNumberValue === '1234') {
+        console.log('you are logged in');
+        window.location.href = './home.html'
+        // step-5: allow user to use the website
+     }else{
+        alert('wrong phone number and pin number')
+     }
     
+
 })
