08f79e0f7edc465be599bc0d1f2bb5244321c6c2
CompleteTechSolution <protech.thecompletetechsolution@gmail.com>
2024-09-20 22:32:40 +0600

    cashout ended

--- cash-Out.js
+++ cash-Out.js
@@ -7,7 +7,7 @@ document.getElementById('btn-cash-money').addEventListener('click', function (ev
     console.log(inputCashOutNumber, inputPin);
     
     if (inputPin === '1234') {
-        const balance = document.getElementById('cash-out-balance');
+        const balance = document.getElementById('cash-out-balance').innerText;
         const balanceNumber = parseFloat(balance);
         const newBalance = balanceNumber - inputCashOutNumber;
         
@@ -18,3 +18,28 @@ document.getElementById('btn-cash-money').addEventListener('click', function (ev
     }
     
 })
+
+document.getElementById('btn-add-money')
+.addEventListener('click', function (event) {
+    event.preventDefault();
+    const inputAddMoney = document.getElementById('input-add-money').value;
+    console.log(inputAddMoney);
+    const inputPinNumber = document.getElementById('input-pin-number').value;
+    console.log(inputPinNumber);
+    if (inputPinNumber === '1234') {
+        console.log('adding money to your account');
+        const balance = document.getElementById('account-balance').innerText
+        console.log(balance);
+        const addMoneyNumber = parseFloat(inputAddMoney);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = balanceNumber + addMoneyNumber;
+        console.log(newBalance);
+        document.getElementById('account-balance').innerText = newBalance
+        
+    }else{
+        alert('Failed to add money to your account')
+    }
+    
+    
+    
+});
